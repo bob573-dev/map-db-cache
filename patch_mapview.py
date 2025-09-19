aed06d9726d60aab940b96e3bd1f5b5380e76afa
B <bob573@atomicmail.io>
2025-09-19 04:54:32 +0300

    fix: retry load tile on mapview

--- patch_mapview.py
+++ patch_mapview.py
@@ -18,6 +18,30 @@ PATCHES = {
     ],
     inspect.getfile(MapView): [
         (
+            'from kivy.uix.image import Image',
+            ('from kivy.uix.image import Image\n'
+             'from kivy.core.image import Image as CoreImage'),
+        ),
+        (
+            'def set_source(self, cache_fn):',
+            ('def set_source(self, cache_fn, retry=3, delay=0.05):\n'
+             '        try:\n'
+             '            CoreImage(cache_fn)'),
+        ),
+        (
+            'self.source = cache_fn',
+            '    self.source = cache_fn',
+        ),
+        (
+            'self.state = "need-animation"',
+            ('    self.state = "need-animation"\n'
+             '        except:\n'
+             '            if retry > 0:\n'
+             '                Clock.schedule_once(lambda dt: self.set_source(cache_fn, retry-1), delay)\n'
+             '            else:\n'
+             '                raise'),
+        ),
+        (
             'if not self.collide_point(*touch.pos):',
             'if not self.collide_point(*touch.pos) or self.disabled:',
         ),
