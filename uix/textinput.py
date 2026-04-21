f5cf80ed34a099e03c665b5785573c58cfcbb56d
B <bob573@atomicmail.io>
2026-04-21 13:02:35 +0300

    copy-paste bubble for inputs

--- uix/textinput.py
+++ uix/textinput.py
@@ -1,12 +1,133 @@
 from kivy.clock import Clock
 from kivy.graphics import Color, Line
 from kivy.properties import NumericProperty, BooleanProperty, ObjectProperty, ListProperty
+from kivy.uix.bubble import Bubble
 from kivy.uix.textinput import TextInput
 
 from consts import DEFAULT_LAT, DEFAULT_LON, INPUT_INCREASE_PNG, INPUT_DECREASE_PNG, FONT_SIZE_SMALL, \
     MIN_CENTER_LATITUDE, MAX_CENTER_LONGITUDE, MAX_CENTER_LATITUDE, MIN_CENTER_LONGITUDE, ERROR_COLOR
 from localization import _
-from .button import ButtonImage
+from tools.binding_manager import BindingManager
+from . import BoxLayoutAutoresized
+from .button import ButtonImage, ButtonColored
+
+
+class BubbleButtonColored(ButtonColored):
+    def __init__(self, **kwargs):
+        kwargs.setdefault('halign', 'left')
+        kwargs.setdefault('valign', 'center')
+        kwargs.setdefault('padding', [12, 0, 0, 0])
+        super().__init__(**kwargs)
+        self.background_color[3] = self.background_color[3] * 0.75
+
+    def on_size(self, *args):
+        self.text_size = self.size
+
+
+class CPBubble(Bubble):
+    def __init__(self, textinput, touch_pos, selection_from, selection_to, **kwargs):
+        kwargs.setdefault('size_hint', (None, None))
+        kwargs.setdefault('show_arrow', True)
+        kwargs.setdefault('arrow_pos', 'top_left')
+        super().__init__(**kwargs)
+        self.textinput: TextInput = textinput
+        self.touch_pos = touch_pos
+        self.selection_from = selection_from
+        self.selection_to = selection_to
+        self._button_size = (self.width, 32)
+        self._bindings = BindingManager()
+        self._root_widget = None
+        self._trigger_restore_selection = Clock.create_trigger(self._restore_selection, 0.15)
+
+        content = BoxLayoutAutoresized(orientation='vertical', spacing=1, padding=1)
+        content.bind(size=self.setter('size'))
+        self.add_widget(content)
+        self._select_all_btn = BubbleButtonColored(text=_('Select all'), on_release=self.select_all, size_hint=(None, None), size=self._button_size)
+        self._cut_btn = BubbleButtonColored(text=_('Cut'), on_release=self.cut, size_hint=(None, None), size=self._button_size)
+        self._copy_btn = BubbleButtonColored(text=_('Copy'), on_release=self.copy, size_hint=(None, None), size=self._button_size)
+        self._paste_btn = BubbleButtonColored(text=_('Paste'), on_release=self.paste, size_hint=(None, None), size=self._button_size)
+
+    def select_all(self, *args):
+        self.selection_from = 0
+        self.selection_to = len(self.textinput.text)
+        self._trigger_restore_selection()
+
+    def cut(self, *args):
+        self.textinput.cut()
+        self.hide()
+
+    def copy(self, *args):
+        self.textinput.copy()
+        self.hide()
+
+    def paste(self, *args):
+        self.textinput.paste()
+        self.hide()
+
+    def hide(self, *args):
+        if self.parent:
+            self.textinput.cancel_selection()
+            self.parent.remove_widget(self)
+            self._bindings.unbind_items()
+
+    def show(self):
+        if not self.parent:
+            self._root_widget = root = self.textinput.get_root_window()
+            root.add_widget(self)
+            self._trigger_restore_selection()
+            self._bindings.bind_item(root, 'on_touch_down', self._on_touch_down_root)
+            self._bindings.bind_item(root, 'size', self.hide)
+            self._update_buttons()
+
+    def _update_buttons(self, *args):
+        self.opacity = 0
+        self.content.clear_widgets()
+        text_available = bool(self.textinput.text)
+        text_selected = (
+            text_available
+            and self.selection_from is not None
+            and self.selection_to is not None
+            and self.selection_from - self.selection_to != 0
+        )
+        all_text_selected = text_selected and abs(self.selection_from - self.selection_to) == len(self.textinput.text)
+        if text_available and not all_text_selected:
+            self.content.add_widget(self._select_all_btn)
+        if text_selected:
+            if not self.textinput.readonly:
+                self.content.add_widget(self._cut_btn)
+            self.content.add_widget(self._copy_btn)
+        if not self.textinput.readonly:
+            self.content.add_widget(self._paste_btn)
+        Clock.schedule_once(self._update_pos)
+
+    def _restore_selection(self, *_):
+        if self.selection_from is None or self.selection_to is None:
+            return
+        self.textinput.select_text(min(self.selection_from, self.selection_to), max(self.selection_from, self.selection_to))
+        self._update_buttons()
+
+    def _on_touch_down_root(self, root, touch):
+        if not self.collide_point(*touch.pos):
+            self.hide()
+
+    def _update_pos(self, *args):
+        touch_x, touch_y = self.touch_pos
+        arrow_relative_x, arrow_relative_y = 15, 8
+        pos_x, pos_y = touch_x - arrow_relative_x, touch_y - self.content.height - arrow_relative_y
+        arrow_vertical = 'top'
+        arrow_horizontal = 'left'
+
+        if pos_x + self.content.width > self.parent.width:
+            pos_x = touch_x - self.content.width + arrow_relative_x
+            arrow_horizontal = 'right'
+
+        if pos_y < 0:
+            pos_y = touch_y + arrow_relative_y
+            arrow_vertical = 'bottom'
+
+        self.pos = (pos_x, pos_y)
+        self.arrow_pos = f'{arrow_vertical}_{arrow_horizontal}'
+        self.opacity = 1
 
 
 class TextInputUnderlined(TextInput):
@@ -15,6 +136,7 @@ class TextInputUnderlined(TextInput):
     def  __init__(self, **kwargs):
         kwargs.setdefault('font_size', FONT_SIZE_SMALL)
         kwargs.setdefault('cursor_color', (0.3, 0.3, 0.3, 1))
+        kwargs.setdefault('use_bubble', False)  # turn off default for mobile
         super().__init__(**kwargs)
         self.background_color = (0, 0, 0, 0)
 
@@ -29,6 +151,30 @@ class TextInputUnderlined(TextInput):
             invalid=self._trigger_update_underline,
         )
         self._trigger_update_underline()
+        self.__bubble = None
+        self._saved_selection = None
+
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            if 'button' in touch.profile and touch.button == 'right':
+                self._saved_selection = (
+                    self.selection_from,
+                    self.selection_to,
+                )
+                self._show_copy_paste_bubble(self.to_window(*touch.pos))
+                return True
+        return super().on_touch_down(touch)
+
+    def _show_copy_paste_bubble(self, touch_pos):
+        if self.__bubble:
+            self.__bubble.hide()
+        self.__bubble = CPBubble(
+            self,
+            touch_pos=touch_pos,
+            selection_from=self._saved_selection[0],
+            selection_to=self._saved_selection[1],
+        )
+        self.__bubble.show()
 
     def _update_underline(self, *_):
         self._line_color.rgba = ERROR_COLOR if self.invalid else (0, 0, 0, 0.8)
